41cf7ec9a5e34660f27ec176f7bf75e0dafd0c87
Lone-Wolf17 <53178226+Lone-Wolf17@users.noreply.github.com>
2020-09-25 20:33:13 +0100

    ** Added a simple Dependency Injection Container
    * started using composer to manage dependencies

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,8 +1,10 @@
 <?php
 
-require 'core/Router.php';
-require 'core/database/Connection.php';
-require 'core/database/QueryBuilder.php';
-$app['config'] = require 'config.php';
 
-$app['database'] = new QueryBuilder(Connection::make($app['config']['database']));
+App::bind('config', require 'config.php');
+
+
+
+App::bind('database', new QueryBuilder(
+    Connection::make(App::get('config')['database'])
+));
